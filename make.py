d9b0c0502ded2fbe8031969af2d4fd8045a3504b
satwik_bh <satwik_bh@hotmail.com>
2016-02-06 02:22:16 +0530

    Removed redundant stuff. Should resolve IAT problem

--- make.py
+++
@@ -1,9 +0,0 @@
-import pefile
-
-pe = pefile.PE('pe.exe')
-
-f = open('pe.txt','w')
-
-print >> f, pe.dump_info()
-
-f.close()
--- run.sh
+++
@@ -1,5 +0,0 @@
-rm pe.exe
-rm temp
-rm temp.enc
-python Packer.py
-python make.py
